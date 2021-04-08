ef8e03921f291849e292520a364c0f8a4f420150
Patrick Stoeckle <patrick.stoeckle@tum.de>
2021-04-08 15:55:56 +0200

    :wrench: Copied code

---
+++ zoom_chat_anonymizer/main.py
@@ -0,0 +1,52 @@
+"""
+Main module.
+"""
+from logging import INFO, basicConfig, getLogger
+from sys import platform, stdout
+from typing import Any, List, Optional, Sequence
+
+from click import Context, Path, echo, group, option
+
+from zoom_chat_anonymizer import __version__
+
+_LOGGER = getLogger(__name__)
+basicConfig(
+    format="%(levelname)s: %(asctime)s: %(name)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=INFO,
+    stream=stdout,
+)
+
+
+def _print_version(ctx: Context, _: Any, value: Any) -> None:
+    """
+
+    :param ctx:
+    :param _:
+    :param value:
+    :return:
+    """
+    if not value or ctx.resilient_parsing:
+        return
+    echo(__version__)
+    ctx.exit()
+
+
+@group()
+@option(
+    "--version",
+    is_flag=True,
+    callback=_print_version,
+    expose_value=False,
+    is_eager=True,
+    help="Version",
+)
+def main_group() -> None:
+    """
+
+    :return:
+    """
+
+
+if __name__ == "__main__":
+    main_group()
