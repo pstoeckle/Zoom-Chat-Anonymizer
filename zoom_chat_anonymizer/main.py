b30daf6af9479053ed4254ec031428a3e045bd87
Patrick Stöckle <patrick.stoeckle@tum.de>
2022-05-20 14:26:11 +0200

    :recycle: Clean up

--- zoom_chat_anonymizer/main.py
+++ zoom_chat_anonymizer/main.py
@@ -1,27 +1,21 @@
 """
 Main module.
 """
-from collections import Counter
-from datetime import datetime
+from datetime import datetime, timedelta
 from logging import INFO, basicConfig, getLogger
 from pathlib import Path
 from re import DOTALL
 from re import compile as re_compile
-from sys import stdout
-from typing import AbstractSet, Any, List, Optional, Sequence
+from typing import AbstractSet, Any, List, Optional
 
-from click import Context, echo, group, option
-from datetime import timedelta
+from click import Context, echo, option
+
 from typer import Argument, Exit, Option, Typer
 from zoom_chat_anonymizer import __version__
-from zoom_chat_anonymizer.classes.artemis import ArtemisStudent
 from zoom_chat_anonymizer.logic.anonymize_chat import anonymize_chat_internal
 from zoom_chat_anonymizer.logic.clean_artemis_file import clean_artemis_file_internal
 from zoom_chat_anonymizer.logic.create_html_from_markdown import (
     create_html_from_markdown_internal,
-)
-from zoom_chat_anonymizer.logic.create_pdf_from_markdown import (
-    create_pdf_from_markdown_internal,
 )
 from zoom_chat_anonymizer.logic.sort_moodle_csv import sort_moodle_csv_internal
 
@@ -58,7 +52,7 @@ basicConfig(
     format="%(levelname)s: %(asctime)s: %(name)s: %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     level=INFO,
-    filename="pdf-link-checker.log",
+    filename="zoom-chat-anomymizer.log",
     filemode="w",
 )
 _INPUT_FOLDER_OPTION = Argument(
@@ -105,7 +99,7 @@ def convert_to_old_format(
         if len(line) > 0:
             if (match := _PATTERN.match(line)) is not None:
                 t = datetime.strptime(match.group(1), "%H:%M:%S")
-                new_t = t + timedelta(hours=13)
+                new_t = t + timedelta(hours=14)
                 new_lines.append(
                     f"{new_t.strftime('%H:%M:%S')} Von {match.group(2)} an Alle : {match.group(3)}\n"
                 )
@@ -171,50 +165,6 @@ def sort_moodle_csv(input_file: Path) -> None:
     Moodle.
     """
     sort_moodle_csv_internal(input_file)
-
-
-# @option(
-#     "--output-file",
-#     "-o",
-#     type=Path(dir_okay=False, resolve_path=True),
-#     default="test.pdf",
-# )
-# @option(
-#     "--latex-header",
-#     "-l",
-#     type=Path(exists=True, dir_okay=False, resolve_path=True),
-#     default="",
-# )
-# @option(
-#     "--markdown-file",
-#     "-m",
-#     multiple=True,
-#     default=None,
-#     type=Path(exists=True, dir_okay=False, resolve_path=True),
-# )
-# @option("--sheet-number", "-n", type=int, prompt=True)
-# @option("--title", "-t", prompt=True)
-# @option("--clean-up", "-c", is_flag=True, default=False)
-def create_pdf_from_markdown(
-    markdown_file: Optional[Sequence[str]],
-    latex_header: str,
-    output_file: str,
-    sheet_number: int,
-    title: str,
-    clean_up: bool,
-) -> None:
-    """
-    Use the markdown description of the exercises to create a PDF.
-    """
-    title = title.replace("&", r"\&")
-    output_path = pathlib_Path(output_file)
-    latex_path = pathlib_Path(latex_header)
-    markdown_paths = (
-        [] if markdown_file is None else [pathlib_Path(m) for m in markdown_file]
-    )
-    create_pdf_from_markdown_internal(
-        clean_up, latex_path, markdown_paths, output_path, sheet_number, title
-    )
 
 
 if __name__ == "__main__":
